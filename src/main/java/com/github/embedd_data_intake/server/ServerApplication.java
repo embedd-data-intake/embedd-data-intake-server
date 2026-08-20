20efc218c3918948dc0b0d62c93e4aef7026f43c
muffindud <ccatlabuga2002@gmail.com>
2026-08-20 14:46:42 +0300

    initial commit

---
+++ src/main/java/com/github/embedd_data_intake/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.embedd_data_intake.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
